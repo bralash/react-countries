b13ac54aaabde29bcf53a2e98fc47f45db01dd60
bralash <emmanuelasaber@gmail.com>
2022-06-28 17:47:51 +0000

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex flex-row justify-between py-7 px-20'>
+        <div className="brand">Where in the world?</div>
+        <div className="control">Dark Mode</div>
+    </div>
+  )
+}
+
+export default Navbar
